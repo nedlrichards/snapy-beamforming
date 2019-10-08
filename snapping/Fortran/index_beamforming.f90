7b00fff11ad5fc1156903b6e9b2cff7a25c18ca1
Edward Richards <edwardlrichards@ucsd.edu>
2019-10-08 14:19:31 -0700

    writting single channel merge routine

--- snapping/Fortran/index_beamforming.f90
+++ snapping/Fortran/index_beamforming.f90
@@ -82,6 +82,153 @@ subroutine numsame(nchan, detection1, detection2, samecount)
   end do
 end subroutine
 
+subroutine cmerge(mshif, ichan, thresh, vbout, kbout, lcurr, lnmax, nchan, nnz)
+  ! merge channels together and return values that are above a threshold
+  ! this is a beamforming routine that takes advantage of the fact that
+  ! the data is sorted channel by channel.
+  !
+  ! ::INPUTS::
+  !
+  ! mshif: nnz vector of value indices, already delay shifted
+  ! ichan: nchan vector marking the start of each channels index and value vector
+  ! nnz: total number of indecies
+  ! nchan: number of data channels
+  ! thresh: beamformer threshold. Only save values which sum GE this value
+  ! lnmax: total allocated beamformer result positions
+
+  integer, intent(in)  :: lnmax, nnz, nchan, thresh
+  integer, intent(in)  :: mshif(nnz), ichan(nchan + 1)
+
+  ! ::OUTPUTS::
+  !
+  ! vbout: vector of beamformer outputs GE thresh
+  ! kbout: index of beamformer outputs
+
+  integer, intent(out) :: kbout(lnmax), vbout(lnmax)
+
+  ! ::COUNTERS::
+  !
+
+  integer :: lcurr
+
+  ! Internal bookeeping variables to move through input index and value pairs
+  ! lcurr: current position in beamformer result array
+  ! mcurr: nchan vector of current index value on each channel
+  ! mrank: nchan vector of ranking of the current index value on each channel
+  ! mpos: nchan vector of position of current index relative to ichan
+
+  integer :: mcurr(nchan), mrank(nchan), mpos(nchan)
+  integer :: munrk(nchan), nsort
+
+  ! setup for merge routine
+  ! filling muncrk leads to a complete sort of mcurr
+  ik = 1
+  do i = 1, nchan
+      mcurr(i) = mshif(ichan(i))
+      mrank(i) = i
+      munrk(i) = i
+      mpos(i) = 0
+  end do
+  nsort = nchan
+
+C sort the start of the buffer
+      call rollsort(mcurr, mrank, munrk, nchan, nsort)
+      jdb = 1
+
+C merge step, combine vals at which indecies are equal
+C initialize beamformer with value from first buffer
+  801 nsort = 0
+C beamformer accumulating variable
+      tmp = vval(ichan(mrank(1)) + mpos(mrank(1)))
+C delay of current beamformer result
+      itmp = mcurr(mrank(1))
+C step channel index
+      mpre = mshif(ichan(mrank(1)) + mpos(mrank(1)))
+      mpos(mrank(1)) = mpos(mrank(1)) + 1
+C check against end condition
+      if( ichan(mrank(1)) + mpos(mrank(1)) .GE.ichan(mrank(1) + 1) )
+     ; then
+          if( mpos(mrank(2)).GT.nnz ) then
+              go to 299
+C first channel is exhausted, but rest of channels are not yet
+          else
+              nsort = 1
+              munrk(1) = 1
+C nnz value of current index is used to mark an exhausted buffer
+              mcurr(mrank(1)) = 2147483647
+              mpos(mrank(1)) = nnz + 1
+          end if
+      else
+C bookeep that a value was pulled
+C check for a discontinuity in index values which requires a sort
+        mcurr(mrank(1)) = mshif(ichan(mrank(1)) + mpos(mrank(1)))
+        if( mpre - mcurr(mrank(1)) .LT. -1 ) then
+            nsort = 1
+            munrk(1) = 1
+        end if
+
+      end if
+
+C add as many buffers as have equal index, then step each buffer
+C this remains unchanged if there is no loop break in 3
+      inum = nchan
+      do 3 i = 2, nchan
+C break loop if the current ranked channel is not the same as first channel
+C nnz value of current index is used to mark an exhausted buffer
+          if( mpos(mrank(i)).GT.nnz .OR. itmp.NE.mcurr(mrank(i)) ) then
+              inum = i - 1
+              goto 201
+          end if
+C accumulate value to beamformer
+          tmp = tmp + vval(ichan(mrank(i)) + mpos(mrank(i)))
+          mpre = mshif(ichan(mrank(i)) + mpos(mrank(i)))
+          mpos(mrank(i)) = mpos(mrank(i)) + 1
+C bookeep that a value was pulled
+C check against end condition
+          if( ichan(mrank(i)) + mpos(mrank(i)).LT.ichan(mrank(i) + 1) )
+     ;    then
+C bookeep that a value was pulled
+              mcurr(mrank(i)) = mshif(ichan(mrank(i)) + mpos(mrank(i)))
+C check for a discontinuity in index values which requires a sort
+              if( mpre - mcurr(mrank(i)) .LT. -1 ) then
+                  nsort = nsort + 1
+                  munrk(nsort) = i
+              end if
+          else
+C nnz value of current index is used to mark an exhausted buffer
+              mcurr(mrank(i)) = 2147483647
+              mpos(mrank(i)) = nnz + 1
+              nsort = nsort + 1
+              munrk(nsort) = i
+          end if
+    3 continue
+
+C Check before adding value to beamformer result
+  201 if( tmp.GE.thresh ) then
+          if ( lcurr.EQ.lnmax ) goto 299
+          lcurr = lcurr + 1
+          vbout(lcurr) = tmp
+          kbout(lcurr) = itmp
+      end if
+
+C Maintain the sorted order of the buffers
+      if( nsort.GT.0 ) then
+           if( jdb .LT. 4) then
+C              print *, munrk
+C              print *, nsort
+           end if
+           call rollsort(mcurr, mrank, munrk, nchan, nsort)
+           if( jdb .LT. 4) then
+C          print *, mcurr
+C          print *, mrank
+           end if
+           jdb = jdb + 1
+       end if
+
+      go to 801
+
+  299 end
+
 subroutine l1norm(nchan, arrs, chani, l1)
   ! compute l1 norm of arrival time difference from median time
   implicit none
